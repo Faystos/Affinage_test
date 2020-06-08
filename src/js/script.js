6be0b30d07dfbc2837ef2ea4cef88587280c19a4
Vladimir <fayst_@bk.ru>
2020-06-08 16:20:02 +0300

    внес правки по коду, добавил на стрницу мелкие элементы по правой стороне.

--- src/js/script.js
+++ src/js/script.js
@@ -12,24 +12,24 @@ btns.forEach(el => {
 });
 
 function startAnimation (el) {  
-  if (el.classList.contains('slider_block__item-left') && (!el.classList.contains('slider_block__item-back'))) {
-    el.classList.add('slider_block__item-front');
+  if (el.classList.contains('slider_block__item--left') && (!el.classList.contains('slider_block__item--back'))) {
+    el.classList.add('slider_block__item--front');
   }
 
-  if (el.classList.contains('slider_block__item-right') && (!el.classList.contains('slider_block__item-front'))) {
-    el.classList.add('slider_block__item-back');
+  if (el.classList.contains('slider_block__item--right') && (!el.classList.contains('slider_block__item--front'))) {
+    el.classList.add('slider_block__item--back');
   } else {
     return;
   }      
 }
 
 function toogleClass (el) {
-  if (el.classList.contains('slider_block__item-back')) {
-    el.classList.remove('slider_block__item-back');
-    el.classList.add('slider_block__item-front');
-  }else if ( el.classList.contains('slider_block__item-front')) {
-    el.classList.remove('slider_block__item-front');
-    el.classList.add('slider_block__item-back');
+  if (el.classList.contains('slider_block__item--back')) {
+    el.classList.remove('slider_block__item--back');
+    el.classList.add('slider_block__item--front');
+  }else if ( el.classList.contains('slider_block__item--front')) {
+    el.classList.remove('slider_block__item--front');
+    el.classList.add('slider_block__item--back');
   }  else {
     return;
   }
