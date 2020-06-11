e66e050d8be788a865cc87ad2ddc9bec308fd289
Vladimir <fayst_@bk.ru>
2020-06-11 19:22:23 +0300

    Доработал слайдер, теперь картинки в слайдах так же переключаются.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./js/script.js"
 import "./less/style.less";
 import arca from './img/arca.jpg';
 import ural from './img/ural.jpg';
+import building from './img/building.jpg';
 
 
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,24 @@
 let btns = document.querySelectorAll('.control_slider__btn');
 let sliderBlockItem = document.querySelectorAll('.slider_block__item');
 
+let imgLeft = document.querySelector('.slider_block__item--left .item__img');
+let imgRight = document.querySelector('.slider_block__item--right .item__img');
+const imgArr = ['arca', 'ural', 'building'];
+let i = 0;
+
+// Событие нажатий кнопки управления слайдером.
 btns.forEach(el => {
   el.addEventListener('click', evt => {
     sliderBlockItem.forEach(it => {
       startAnimation(it);
-      toogleClass(it);
-    })    
+      toogleClass(it);      
+    });
+    swapImagSlide(imgLeft, imgArr);
+    swapImagSlide(imgRight, imgArr);    
   })
 });
 
+// Инициация запуска анимации смены слайдов.
 function startAnimation (el) {  
   if (el.classList.contains('slider_block__item--left') && (!el.classList.contains('slider_block__item--back'))) {
     el.classList.add('slider_block__item--front');
@@ -21,6 +30,7 @@ function startAnimation (el) {
   }      
 }
 
+// смена классов при акцивации слайда.
 function toogleClass (el) {
   if (el.classList.contains('slider_block__item--back')) {
     el.classList.remove('slider_block__item--back');
@@ -32,3 +42,11 @@ function toogleClass (el) {
     return;
   }
 }
+
+// смена изображения в слайдах.
+function swapImagSlide (imgSlide, arr) {
+  if (i === arr.length) i = 0;  
+  imgSlide.src = `./img/${arr[i]}.jpg`;    
+  i++;    
+}
+
