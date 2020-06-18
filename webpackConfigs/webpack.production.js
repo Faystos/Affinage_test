64563f3d7cb74f4429731682eee298be14827edc
Vladimir <fayst_@bk.ru>
2020-06-18 19:10:28 +0300

    Поправил конфиг, webpaca в режиме продакшен

--- webpackConfigs/webpack.production.js
+++ webpackConfigs/webpack.production.js
@@ -1,29 +1,29 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = () => ({
-    module: {
-        rules: [
-            {
-                test: /\.less$/,
-                use: [                    
-                    MiniCssExtractPlugin.loader,
-                    {
-                      loader: "css-loader",                      
-                    },
-                    {
-                      loader: "postcss-loader",
-                      options: {                        
-                        config: { path: `webpackConfigs/postcss.config.js` }
-                      }
-                    },
-                    {
-                      loader: "less-loader",                      
-                    }
-                  ],
-            },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [                    
+          {                      
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",                      
+          },
+          {
+            loader: "postcss-loader",
+            options: {                        
+              config: { path: `webpackConfigs/postcss.config.js` }
+            }
+          },
+          {
+            loader: "less-loader",                      
+          }
         ],
-    },
-    plugins: [new MiniCssExtractPlugin({
-        filename: "./css/[name].css"
-    })],
+      },
+    ],        
+  },
+  plugins: [new MiniCssExtractPlugin()],   
 });
